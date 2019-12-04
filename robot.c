632cd4242df1f4199dd23dc6e31995f90c7d33f4
Mohammadi <mohammap22@egr-linux-6413.campus.up.edu>
2019-12-04 15:03:42 -0800

    Made robot move

--- robot.c
+++ robot.c
@@ -33,6 +33,7 @@ void robotPrintAscii(void)
   int i;
   char* blank = "                              ";
   blank = blank +30;
+  for (i = 0; i < 3; i++) {
   printf("\n\n"
 	 "UWR: Unidentified Wheeled Robot\n"
 	 "%s    i_i    \n"
@@ -41,6 +42,7 @@ void robotPrintAscii(void)
 	 "%s   d   b   \n",blank, blank, blank, blank);
 
   blank = blank - 10;
+}
 }
 
 /**
