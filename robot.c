076ce145fce192071fedd0d5baa2c4aec76bcb33
Lee <lees22@egr-linux-6423.campus.up.edu>
2019-12-04 14:52:05 -0800

    Animated robot

--- robot.c
+++ robot.c
@@ -33,14 +33,16 @@ void robotPrintAscii(void)
   int i;
   char* blank = "                              ";
   blank = blank +30;
-  printf("\n\n"
-	 "UWR: Unidentified Wheeled Robot\n"
+  
+  printf("UWR: Unidentified Whelled Robot\n");
+  for(i = 0; i < 3; i++){
+    printf("\n\n"
 	 "%s    i_i    \n"
 	 "%s   [@_@]   \n"
 	 "%s  /|___|\\ \n"
 	 "%s   d   b   \n",blank, blank, blank, blank);
-
-  blank = blank - 10;
+    blank = blank -10;
+  }
 }
 
 /**
